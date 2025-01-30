71e0c63a73023a31e3d15074816d85144f2fb7d6
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-01-30 12:21:21 +0530

    refactor: separate image_to_ascii into smaller functions

--- ascii_forge/__main__.py
+++ ascii_forge/__main__.py
@@ -4,11 +4,10 @@ from ascii_forge.config import COLOR_MAP
 
 @click.command()
 @click.argument("image_path", type=click.Path(exists=True))
-@click.option("--width", "-w", default=100, type=int, help="Width of ASCII ouput")
+@click.option("--width", "-w", default=100, type=int, help="Width of ASCII ouput (may improve clarity)")
 @click.option("--invert", "-i", is_flag=True, help="Invert ASCII output")
 @click.option("--color", "-c", default=None, type=click.Choice(COLOR_MAP.keys()), help="Color of ASCII output")
 def cli(image_path, width, invert, color):
     """Convert an IMAGE to ASCII Art"""
     ascii_art = image_to_ascii(image_path, width, invert, color)
-
     click.echo(ascii_art)
--- ascii_forge/converter.py
+++ ascii_forge/converter.py
@@ -1,20 +1,23 @@
 from PIL import Image, ImageFile
 from ascii_forge.config import ASCII_CHARS, COLOR_MAP
 
-def apply_monochrome_color(ascii_art, color):
+def apply_monochrome_color(ascii_art: str, color: str) -> str:
     return color + ascii_art
 
-def image_to_ascii(image_path: str, width: int = 100, invert: bool = False, color=None) -> ImageFile.ImageFile:
-    image = Image.open(image_path)
+def resize_image(image: ImageFile.ImageFile, new_width: int) -> ImageFile.ImageFile:
     aspect_ratio = image.height / image.width
-    new_height = int(width * aspect_ratio * 0.5)
+    new_height = int(new_width * aspect_ratio * 0.5)    
+    return image.resize((new_width, new_height))
 
-    image = image.resize((width, new_height)).convert('L')
+def grayify_image(image):
+    return image.convert('L')
+
+def image_to_ascii(image_path: str, width: int = 100, invert: bool = False, color=None) -> str:
+    image = Image.open(image_path)
+    image = resize_image(image, width)
+    image = grayify_image(image)
     
-    if invert:
-        ASCII_CHARS_USED = ASCII_CHARS[::-1]
-    else:
-        ASCII_CHARS_USED = ASCII_CHARS
+    ASCII_CHARS_USED = ASCII_CHARS[::-1] if invert else ASCII_CHARS
 
     pixels = image.getdata()
     ascii_str = "".join(ASCII_CHARS_USED[pixel // 25] for pixel in pixels)

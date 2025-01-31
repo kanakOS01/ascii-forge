6e5d2ecfecb6417cd490d675f912da7f26736b69
Kanak Tanwar <kanaktanwarpro@gmail.com>
2025-01-31 18:04:03 +0530

    feat: add --save option

--- ascii_forge/__main__.py
+++ ascii_forge/__main__.py
@@ -1,3 +1,4 @@
+from time import time
 import click
 from ascii_forge.converter import image_to_ascii
 from ascii_forge.config import COLOR_MAP
@@ -7,7 +8,16 @@ from ascii_forge.config import COLOR_MAP
 @click.option("--width", "-w", default=100, type=int, help="Width of ASCII ouput (may improve clarity)")
 @click.option("--invert", "-i", is_flag=True, help="Invert ASCII output")
 @click.option("--color", "-c", default=None, type=click.Choice(COLOR_MAP.keys()), help="Color of ASCII output")
-def cli(image_path, width, invert, color):
+@click.option("--save", "-s", default=None, type=str, help="Save ASCII output to a file")
+def cli(image_path, width, invert, color, save):
     """Convert an IMAGE to ASCII Art"""
     ascii_art = image_to_ascii(image_path, width, invert, color)
-    click.echo(ascii_art)
+
+    if save:
+        print(save)
+        file_path = save 
+        with open(file_path, 'w') as f:
+            f.write(ascii_art)
+        click.echo(f"ASCII Art save to {file_path}")
+    else:
+        click.echo(ascii_art)
